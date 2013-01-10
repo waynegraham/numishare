68737903a12f4948b584318b16e4b1efa7adc63b
Ethan Gruber <ewg4xuva@gmail.com>
2013-01-10 16:05:59 -0500

    enabling fancybox

--- cocoon/javascript/display_functions.js
+++ cocoon/javascript/display_functions.js
@@ -5,6 +5,7 @@ Library: jQuery
 Description: Rendering graphics based on hoard counts
 ************************************/
 $(document).ready(function () {
+	$('a.thumbImage').fancybox();
 	//enable basic query form
 	
 	/**
